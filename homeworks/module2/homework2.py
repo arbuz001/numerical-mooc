f288c8a35215f120b504e6a844632242deb6bc54
alexey.arbuzov <alexey.arbuzov@gmail.com>
2014-10-05 23:31:31 -0700

    initial version of homework2.py

--- homeworks/module2/homework2.py
+++ homeworks/module2/homework2.py
@@ -10,13 +10,20 @@ nx = 51  # try changing this number from 41 to 81 and Run All ... what happens?
 L = 11.0
 dx = L / (nx - 1)
 dt = .001
-nt = int(20 / 60.0 / dt)
+n_sec = 0.0
+nt = int(n_sec / 60.0 / dt)
 Vmax = 80.
 Rhomax = 250.
 
 x = numpy.linspace(0, L, nx)
 rho = numpy.ones(nx) * 10
 rho[10:20] = 50.0  # as per our I.C.s
+
+# at time t = 0
+v = Vmax * (1 - rho / Rhomax)
+v_min = min(v)
+v_min_ms = v_min * 1000.0 / 3600.0
+print 'minimum spped at time t = 0 in m/s:', v_min_ms
 
 # print(rho)
 # plt.plot(x, rho, color='#003366', ls='--', lw=3)
@@ -27,6 +34,12 @@ for n in range(1, nt):
     rho[0] = 10.0
     for i in range(1, nx):
         rho[i] = rhon[i - 1] - dt / dx * Vmax * (rhon[i] - rhon[i - 1]) * (1 - rhon[i] / Rhomax)
+
+# at time t = 2 sec
+v = Vmax * (1 - rho / Rhomax)
+v_min = min(v)
+v_min_ms = v_min * 1000.0 / 3600.0
+print 'minimum spped at time t =', n_sec, 'in m/s:', v_min_ms
 
 plt.plot(x, rho, color='#003366', ls='--', lw=3)
 # plt.ylim(0, 2.5);
